8a15c449a8f525b5f77c759f98e81467c3693e55
Christopher Gabaldon <129426874+chris-gabaldon@users.noreply.github.com>
2025-03-20 09:01:25 -0300

    xample_of_use.py

--- Algorithm/example_of_use.py
+++ Algorithm/example_of_use.py
@@ -5,38 +5,33 @@ Created on Tue Mar 18 19:57:03 2025
 @author: C_Gab
 """
 
-# simple tutorial to use the ado algorithm with a gaussian prior
+# Step by step tutorial for using the ado algorithm with a gaussian prior in your own experiment.
 
-# step 1 : import the Experiment and Prior classes
+# step 1 : import the Experiment and Prior classes.
 from priors import *
 from ADO_Experiment import *
 
-# step 2: generate an instance of the experiment
-
+# step 2: generate an instance of the experiment.
 exp= Experiment()
 
-# step 3: set the prior , if non informative prior is needed, skip this step
-
+# step 3: set the prior , if non informative prior is needed, skip this step.
 prior = Prior() 
 p = prior.set_prior_gauss(expan,D,k) # calculate the prior based on gaussian curves, to use a non informative prior coment this line
 exp.set_prior(p)
 
-# step 4: define your discretization and generate the model , set the ones appropiated for your experiment
-
-n=10 # number of design points
+# step 4: define your discretization and generate the model , set the n and k values appropiated for your experiment.
+n=10 # number of design points of your experiment
 k=100 # number of likelihood bins
 exp.generate(n, k)
 
-# step 5 : the algorithm selects a design point 'd' to meassure
-
+# step 5 : the algorithm selects a design point 'd' to meassure.
 d=exp.ADOchoose()
 
-# step 6 : meassure at this point, and update the model using the experiments result (if the experiment has binary results y=1 or y=0)
-
+# step 6 : meassure in this design point, and update the model using the result of the meassure (if the experiment has binary results y=1 or y=0).
 y=1 # or may be y=0 
 exp.update(d,y)
 
-# step 7 : iterate step 5 and 6 as many times as neccesary. For a new experiment (for example a new participant repeat the step 4)
+# step 7 : iterate step 5 and 6 as many times as neccesary. For a new experiment (for example a new participant repeat the step 4).
 
 
     
