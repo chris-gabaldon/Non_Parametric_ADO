21c6d40626ad9e2273fbf2d709ab3a4dc22a55fc
Christopher Gabaldon <129426874+chris-gabaldon@users.noreply.github.com>
2025-05-06 22:14:32 -0300

    ground_truth.py

--- Algorithm/MSE_analysis.py
+++ Algorithm/MSE_analysis.py
@@ -3,6 +3,7 @@ Created on Tue Mar 18 14:23:50 2025
 
 @author: C_Gab
 """
+
 #%%  Necessary functions and third-party libraries
 import math
 import random   
@@ -22,15 +23,11 @@ n=10 # number of design
 d=[0,1,2,3,4,5,6,7,8,9] # design points
 D=len(d)
 expan=d[-1]
-# true=np.array([10,9.9,9,8,4,1,0.4,0.1,0.11,0.1])/10
-# true=np.array([10,10,9,7,5,3,2,1,0.01,0])/10 # soft logis model
-# true=np.array([10,10,9.9,9,5,3,1,0.01,0,0])/10 # saturated logis model
+
 true=np.array([1 , 0.6 , 0.3  , 0.1, 0.05 , 0. , 0.0 , 0.0 ,0,0]) # saturated exponential model
-# true=np.linspace(1,0,10) # linear model
-# true=np.array([1 , 0.6 , 0.4 , 0.25, 0.15 , 0.05 , 0.02 , 0.0 ,0,0]) # soft exponential model
+
 true = 1 - true # to obtain an ascending monotony (which will then be reversed)
-true=np.array([0.1       , 0.48362192, 0.70372631, 0.83001196, 0.90246878,
-        0.94404113, 0.96789341, 0.98157873, 0.98943073, 0.99393585])
+
 plt.plot(d,true,'o')
 
 
@@ -56,7 +53,7 @@ plt.legend(fontsize=12)
 plt.show()
 
 
-#%% Simulation of the ground truth model
+#%% Simulation of the ground truth model with for example non informative prior
 
 list_d_ado=[] # store the designs seleceted to create an histogram
 
@@ -65,19 +62,8 @@ trials=300 # number of trials
 
 for i in range (trials):
     
-    '''
-    The first `if` implements a full sweep over all design points initially.
-    `n_first_loops` sets how many times this sweep should occur.
-
-    '''
-    
-    n_first_loops=2
-    ff=[range(D)[mm % D] for mm in range(n_first_loops*D)] # a flat list with the design ordered
-    if i<n_first_loops*D:
-        
-        d1=ff[i]
-    else:       
-        d1=exp.ADOchoose() # if the first sweep finish, the algorithm choose a design
+      
+    d1=exp.ADOchoose() # if the first sweep finish, the algorithm choose a design
         
     list_d_ado.append(d1)
     aux=np.random.random()
@@ -145,213 +131,95 @@ plt.legend()
 
 '''
 In this cell, we perform several simulations of 300 trials each and calculate the MSE 
-with respect to the ground truth curves for each of the studied cases: non-informative prior,
-Gaussian prior, and random.
+with respect to the ground truth curves 
 '''
 
 
-exp=Experiment()
-repetitions=50 # number of simulations
-trials=300
+expan = 1000 # the final time
+n = 10 # number of design points
+k = 100 # number of likelihood points
 
-##### ado algorithm with non informative prior
+print("Initializing experiment")
+print("")
+
+exp = Experiment()
+exp.generate(n,k) # generate the bins priors considering all monotonic curves equally probable
+aux = np.linspace(0, expan, n) # design points
 
 
-list_totals_mse_ado=[]
-list_totals_y=[]
-totals_d_ado=[]
-exp.generate(n, k)
+true = 1 - 0.9* np.exp(-aux*0.005) #ground truth
 
-p_prior = exp.p.copy() 
-mat_prior = exp.mat.copy()
-invmat_prior = exp.invmat.copy()
-print('Non informative')
+trials = 300
+ind =  50 # number of runs used to average the MSE vs. trial curve
+xs = np.zeros(shape=(trials+1, 2,ind)) # to store each trial number
+MSEs = np.zeros(shape=(trials+1,2,ind)) # to store the MSE vs trials values
+infoGained = np.zeros(shape=(trials+1,2,ind)) # to store the information gained vs trials values
+designs = []
+results = []
 
-for rep in range(repetitions):
-    exp.reset()
-    exp.set_prior_mat(p_prior, mat_prior, invmat_prior)    
-    if rep % (repetitions/10)==0:
-        print(rep)
-    list_d_ni=[]
-    list_mse_freq_ado=[]
-    list_y=[]
-    corrects=np.zeros_like(d)
-    totals=np.zeros_like(d)
-
-    for i in range (trials):
-        # initial sweep
-        n_first_loops=2
-        ff=[range(D)[mm % D] for mm in range(n_first_loops*D)]
-        if i<n_first_loops*D:
-            d1=ff[i]
-        else:       
-            d1=exp.ADOchoose()
-        list_d_ni.append(d1)
-        aux=np.random.random()
-        if aux<=true[d1]: # simulate the response using the ground truth
-            y=1
-            corrects[d1]+=1
-        else:
-            y=0
-        exp.update(d1,y)
-        list_y.append(y)
-        totals[d1]+=1
-        estimation_ado=corrects/(totals+eps)
-        mse_freq=np.mean((estimation_ado-true)**2)
-        list_mse_freq_ado.append(mse_freq)
-        
-    list_totals_mse_ado.append(list_mse_freq_ado)
-    totals_d_ado.append(list_d_ni)
-    list_totals_y.append(list_y)
-
-##### random algorithm
-print ('random')
-list_totals_mse_random=[]
-exp.generate(n, k)
+for jj in range(2): # to perform the simulations first choosing designs according to our method and then randomly
+    if jj==0:
+        print("simulating ADO experiment:")
+        print("")
+    else:
+        print("simulating random design experiment:")
+        print("")
+    for ii in range(ind): 
+        print("run number:",ii)
+        exp.reset()
    
-totals_d_random=[]
-for repp in range(repetitions):
-    exp.reset()
-    list_mse_freq_random=[]
-    corrects=np.zeros_like(d)
-    totals=np.zeros_like(d)
-    listas_d_r=[]
-    for ii in range (trials):
-        n_first_loops=2
-        ff=[range(D)[mm % D] for mm in range(n_first_loops*D)]
-        if i<n_first_loops*D:
-            
-            d1=ff[i]
-        else:       
-            d1=exp.RANDchoose()
-        listas_d_r.append(d1)
-        aux=np.random.random()
-        if aux<=true[d1]:
-            y=1
-            corrects[d1]+=1
-        else:
-            y=0
-            corrects[d1]+=0
-        exp.update(d1,y)
-        totals[d1]+=1
-        estimation_freq=corrects/(totals+eps)
-        mse_freq=np.mean((estimation_freq-true)**2)
-        list_mse_freq_random.append(mse_freq)    
-    list_totals_mse_random.append(list_mse_freq_random)
-    totals_d_random.append(listas_d_r)
-
-mean_ado_ni=np.mean(list_totals_mse_ado,axis=0)
-std_ado_ni=np.std(list_totals_mse_ado,axis=0)
-mean_random=np.mean(list_totals_mse_random,axis=0)  
-std_random=np.std(list_totals_mse_random,axis=0)
-
-# plt.title('Random')
-# plt.hist(totals_d_random)
-# plt.show()
-# plt.title('Ado - Non informative')
-# plt.hist(totals_d_ado)
-# plt.show()
-
-
-##### ado algorithm with gaussian prior
-
-print('Gaussian')
-
-exp=Experiment()
-exp.generate(D,k)
-
-list_totals_mse_ado_g=[]
-list_d_ado=[]
-list_totals_y_g=[]
-
-prior = Prior() 
-p = prior.set_prior_gauss(expan,n,k) # calcualte the prior based on gaussian curves
-exp.set_prior(p) # set the bins priors as the one calculated above 
-
-p_prior = exp.p.copy() 
-mat_prior = exp.mat.copy()
-invmat_prior = exp.invmat.copy()
-
-totals_d_ado_g=[]
-for rep in range(repetitions):
-    exp.reset()
-    exp.set_prior_mat(p_prior, mat_prior, invmat_prior)    
-    if rep % (repetitions/10)==0:
-        print(rep)
-    list_d_g=[]
-    list_mse_freq_ado=[]
-    list_y_g=[]
-
-    corrects=np.zeros_like(d)
-    totals=np.zeros_like(d)
-
-    for i in range (trials):
-        n_first_loops=2
-        ff=[range(D)[mm % D] for mm in range(n_first_loops*D)]
-        if i<n_first_loops*D:
-            
-            d1=ff[i]
-        else:       
-            d1=exp.ADOchoose()
-        list_d_g.append(d1)
-        aux=np.random.random()
-        if aux<=true[d1]: # simulate the response using the ground truth
-            y=1
-            corrects[d1]+=1
-        else:
-            y=0
-        exp.update(d1,y)
-        totals[d1]+=1
-        list_y_g.append(y)
-        estimation_freq=corrects/(totals+eps)
-        mse_freq=np.mean((estimation_freq-true)**2)
-        list_mse_freq_ado.append(mse_freq)
-    list_totals_mse_ado_g.append(list_mse_freq_ado)
-    totals_d_ado_g.append(list_d_g)
-    list_totals_y_g.append(list_y_g)
-
-mean_ado_g=np.mean(list_totals_mse_ado_g,axis=0)
-std_ado_g=np.std(list_totals_mse_ado_g,axis=0)
-
-
-# plt.title('Ado - Gaussian')
-# plt.hist(totals_d_ado_g)
-# plt.show()
-
-# the 0.434 factor corresponds to the correct log transformation for the error bars
-
-plt.errorbar(range(trials), np.log10(mean_ado_ni), yerr=0.434* std_ado_ni/(np.sqrt(repetitions)), label='ADO-Non Informative',c='blue')
-plt.errorbar(range(trials), np.log10(mean_random), yerr=0.434* std_random/(np.sqrt(repetitions)), label='Random',c='orange')
-plt.errorbar(range(trials), np.log10(mean_ado_g), yerr=0.434* std_ado_g/(np.sqrt(repetitions)), label='ADO-Gaussian',c='green')
-
-plt.xlabel('Trial', fontsize=14)
-plt.ylabel('log(MSE)', fontsize=14)
-plt.legend()
-plt.show()
-
-#%%histograms time evolution (set an emergent figure window )
-
-data =totals_d_ado_g[16]
-
-# timelaps
-num_intervals = 30
-interval_length = len(data)//num_intervals
-
-def update_hist(num, data, bins, patch):
+        x = [0]
+        y = []
+        estimated = exp.values*exp.p
+        estimated = estimated.sum(axis = 1) # estimated model at the begining
+        err = estimated - true
+        aux_err = np.mean(err*err) # error at the begining
+        MSE = [aux_err]
+        info = [exp.infoProgress()]
+        for i in range(trials):  
+            if jj == 0: 
+                aux1 = exp.ADOchoose()  # select the design using our ADO algorithm
+                designs.append(aux1)
+            else:
+                aux1 = exp.RANDchoose() # select a random design
+                
+            aux2 = random.random() # choose a random number between 1 and 0
+            if aux2 <= true[aux1]: # select if the result is 0 or 1 according to the probability given by the ground truth
+                exp.update(aux1,1)
+            else:
+                exp.update(aux1,0)  
+            results.append(aux2 <= true[aux1])
+            x.append(i+1)
+            estimated = exp.values*exp.p
+            estimated = estimated.sum(axis = 1)
+            # estimated=exp.values[np.argmax(exp.p,axis=1)]
+            err = estimated - true
+            aux_err = np.mean(err**2)
+            MSE.append(aux_err)
+            info.append(exp.infoProgress())
+        aux1 = exp.ADOchoose()  
+        designs.append(aux1)    
+        MSEs[:,jj,ii] = MSE
+        xs[:,jj,ii] = x
+        infoGained[:,jj,ii] = info
+ 
+#### Visualization
+colors = ["green", "blue"]
+stratName = ["ADO","Random"]
+# plot MSE vs trial
+plt.figure(2)
+for i in range(2):
+    plt.errorbar(xs[:,i].mean(axis = 1), np.log10(MSEs[:,i].mean(axis = 1)), yerr=0.434* MSEs[:,i].std(axis = 1)/MSEs[:,i].mean(axis = 1)/np.sqrt(ind), label=stratName[i],c=colors[i])
+    plt.xlabel('Trial', fontsize=14)
+    plt.ylabel('MSE', fontsize=14)
+    plt.legend(stratName)
     
-    patch[0].remove()  
-    patch[0]=plt.title(f'Trials: {num * interval_length}')
-    patch[0] = plt.hist(data[:num * interval_length], bins=bins, edgecolor='black',alpha=0.75,color='blue')[2]
+# plot information gain vs trial    
+plt.figure(20)
+for i in range(2):
+    plt.errorbar(xs[:,i].mean(axis = 1), infoGained[:,i].mean(axis = 1), yerr=infoGained[:,i].std(axis = 1)/np.sqrt(ind), label=stratName[i],c=colors[i])
+    plt.xlabel('Trial', fontsize=14)
+    plt.ylabel('InfoGained', fontsize=14)
+    plt.legend(stratName)
 
-fig, ax = plt.subplots()
-bins = range(12)
-ax.set_xlim(min(data), max(data)+1)
-
-patch = [ax.hist(data[:interval_length], bins=bins, alpha=0.75)[2]]
-
-# Crete the animation
-ani = animation.FuncAnimation(fig, update_hist, fargs=(data, bins, patch)
-                              ,frames=num_intervals, repeat=False)
-ani.save('animacion.mp4', writer='ffmpeg')
-plt.show()
 
