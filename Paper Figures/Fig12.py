a4e83d715143d1d89603f731897d53c36f5bbe2c
Christopher Gabaldon <129426874+chris-gabaldon@users.noreply.github.com>
2025-05-25 00:54:15 -0300

    Update Fig12.py

--- Paper Figures/Fig12.py
+++ Paper Figures/Fig12.py
@@ -15,6 +15,8 @@ from priors import *
 
 
 list_priors=['exp','pow','logis','cuadra','gauss']
+
+
 for prior_type in list_priors: 
     print('Prior type=',prior_type)
     
@@ -51,7 +53,7 @@ for prior_type in list_priors:
                 mu = 0 
                 sigma = np.random.uniform(expan/10, expan/5) 
                 true = 1-np.exp(-0.5 * ((aux - mu) / sigma) ** 2)  
-        # true = true / np.max(true)  # Normalización para garantizar que los valores estén entre 0 y 1
+                true = true / np.max(true)  # Normalización para garantizar que los valores estén entre 0 y 1
                 
             
             trueS[:,i] = true # save for later use                          
@@ -59,15 +61,15 @@ for prior_type in list_priors:
         return trueS
     
     
-    def MSEvsK(k, trials, realizations, trueS,prior_type): # repeats is the number of trials and realizations is the number of times the experiment is run
+    def MSEvsK(k, trials, realizations, trueS): # repeats is the number of trials and realizations is the number of times the experiment is run
         global expan
         global n
         
         exp = Experiment()
         exp.generate(n,k)
-    
+
         print('\nSetting prior using k =', k)
-    
+
         if prior_type=='logis':
             prior = Prior() 
             p = prior.set_prior_logis(expan,n,k) 
@@ -81,7 +83,7 @@ for prior_type in list_priors:
             prior = Prior() 
             p = prior.set_prior_cuadra(expan,n,k)
             exp.set_prior(p) 
-    
+
         elif prior_type=='exp':
             prior = Prior() 
             p = prior.set_prior_exp(expan,n,k)
@@ -91,21 +93,26 @@ for prior_type in list_priors:
             prior = Prior() 
             exp.generate(n,k) # non informative prior
             
-        p_prior = exp.p.copy() 
+        
+        print('\nSetting prior using k =', k)
+        # exp.set_prior(p)
+
+        p_prior = exp.p.copy()
         mat_prior = exp.mat.copy()
         invmat_prior = exp.invmat.copy()
+        
         MSEs = np.zeros(shape=(trials,realizations))
         designs = np.zeros(shape=(trials,realizations))
         results = np.zeros(shape=(trials,realizations))
         
-
+        MSEs3 = np.zeros(realizations)
         for ii in range(realizations):
-            # print('\rRealization number: ' + str(ii), end='', flush=True)
+            print('\rRealization number: ' + str(ii), end='', flush=True)
             exp.reset()
             exp.set_prior_mat(p_prior.copy(), mat_prior.copy(), invmat_prior.copy())
             
             true = trueS[:, ii]
-    
+
             MSE = []
             design = []
             result = []
@@ -127,34 +134,110 @@ for prior_type in list_priors:
             MSEs[:,ii] = MSE
             designs[:,ii] = design
             results[:,ii] = result
+        
+        ######
+            # estimate and error with freADO
+            number_of_designs = np.array([len(designs[:,ii][designs[:,ii] == j])  for j in range(n)])
+            number_of_positive_results = np.array([sum(results[:,ii][designs[:,ii] == j])  for j in range(n)])
             
-        return MSEs.mean(), MSEs.std()/np.sqrt(realizations), designs, results
+            number_of_positive_results[number_of_designs==0] = 1
+            number_of_designs[number_of_designs==0] = 1
+            estimated_fre_ADO = number_of_positive_results/number_of_designs
+            
+            err = estimated_fre_ADO - true
+            aux_err = np.mean(err*err)
+            MSEs3[ii] = aux_err
+            
+        # ####### run new simulations with k 200 and saved desings and results        
+        MSEs2 = np.zeros(shape=(trials,realizations))    
+        print("\nGenerating simulations with obtained results")
+        exp = Experiment()
+        exp.generate(n,200)
+
+        print('\nSimulation usigin k = 200 and saved results')
+        for ii in range(realizations):
+            # print('\rRealization number: ' + str(ii), end='', flush=True)
+            exp.reset()
+            exp.set_prior_mat(p_prior2.copy(), mat_prior2.copy(), invmat_prior2.copy())
+            true = trueS[:,ii]      
+            MSE = []
+        
+            for i in range(trials):           
+                aux1 = designs[i,ii]
+                result = results[i,ii]
+                exp.update(aux1.astype(int),result.astype(int))        
+                estimated = exp.values*exp.p
+                estimated = estimated.sum(axis = 1)
+                err = estimated - true
+                aux_err = np.mean(err*err)
+                MSE.append(aux_err)
+                
+            MSEs2[:,ii] = MSE  
+            
+        return MSEs2.mean(axis = 1)[-1], MSEs2.std(axis = 1)[-1]/np.sqrt(realizations)\
+           ,MSEs3.mean(), MSEs3.std()/np.sqrt(realizations), designs, results
+          
+        return MSEs3.mean(), MSEs3.std()/np.sqrt(realizations), designs, results
     
     ##################  Simulation 
+   
     # prior with k 200
-    print("\nGenerating prior with k 200")
+  
     expan = 1000
-    n = 10
+    n = 10 
+
+    k_prov = 200     
+    exp = Experiment()
+    exp.generate(n,k_prov)
+
+    if prior_type=='logis':
+        prior = Prior() 
+        p = prior.set_prior_logis(expan,n,k_prov) 
+        exp.set_prior(p) # set the bins priors as the one calculated above 
+    elif prior_type=='pow':
+        prior = Prior() 
+        p = prior.set_prior_pow(expan,n,k_prov)
+        exp.set_prior(p) # set the bins priors as the one calculated above 
+        
+    elif prior_type=='cuadra':
+        prior = Prior() 
+        p = prior.set_prior_cuadra(expan,n,k_prov)
+        exp.set_prior(p) 
+ 
+    elif prior_type=='exp':
+        prior = Prior() 
+        p = prior.set_prior_exp(expan,n,k_prov)
+        exp.set_prior(p) 
+        
+    elif prior_type=='gauss':
+        prior = Prior() 
+        exp.generate(n,k_prov) # non informative prior
     
+        exp.set_prior(p)
+    p_prior2 = exp.p.copy()
+    mat_prior2 = exp.mat.copy()
+    invmat_prior2 = exp.invmat.copy()
+
     ### 
-    
+    # ks = [5,10] 0
     MSEmeans = []
     MSEstds = []
     MSEmeans_freADO = []
     MSEstds_freADO  = []
-    
+
     All_results = []
     All_designs = []
-    
+
     trials = 300
     realizations = 320
     ks = [5,10,20,40,100,200]
+
     trueS = generate_trueS(realizations,prior_type)
-    
+
     # ks = range(5,66,20)
     for k in ks:
         print('\nSimulating for k=', k)
-        simulation = MSEvsK(k,trials,realizations, trueS,prior_type)
+        simulation = MSEvsK(k,trials,realizations, trueS)
         MSEmeans.append(simulation[0])    
         MSEstds.append(simulation[1])
         # MSEmeans_freADO.append(simulation[2])    
@@ -163,11 +246,12 @@ for prior_type in list_priors:
         All_results.append(simulation[3])
         
         print( k, simulation[0], simulation[1]) 
-    
+
     colors = ['blue','green']
     plt.figure(2)
-    plt.title(f'prior ={prior_type}')
     plt.errorbar(ks, MSEmeans,MSEstds,c=colors[0])
+
+
     
     
     # =============================================================================
@@ -175,12 +259,35 @@ for prior_type in list_priors:
     # =============================================================================
     expan = 1000 # the final time
     n = 10 # number of design points
-
+    k=200
     
     
     exp = Experiment()
     exp.generate(n,k) # generate the bins priors considering all monotonic curves equally probable
     aux = np.linspace(0, expan, n) # design points
+    
+    if prior_type=='logis':
+        prior = Prior() 
+        p = prior.set_prior_logis(expan,n,k) 
+        exp.set_prior(p) # set the bins priors as the one calculated above 
+    elif prior_type=='pow':
+        prior = Prior() 
+        p = prior.set_prior_pow(expan,n,k)
+        exp.set_prior(p) # set the bins priors as the one calculated above 
+        
+    elif prior_type=='cuadra':
+        prior = Prior() 
+        p = prior.set_prior_cuadra(expan,n,k)
+        exp.set_prior(p) 
+
+    elif prior_type=='exp':
+        prior = Prior() 
+        p = prior.set_prior_exp(expan,n,k)
+        exp.set_prior(p) 
+        
+    elif prior_type=='gauss':
+        prior = Prior() 
+        exp.generate(n,k) # non informative prior
     
     p_prior = exp.p.copy() 
     mat_prior = exp.mat.copy()
@@ -188,9 +295,9 @@ for prior_type in list_priors:
     
     
     trials = 300
-    ind =  320 # number of runs used to average the MSE vs. trial curve
+    ind =  realizations # number of runs used to average the MSE vs. trial curve
     xs = np.zeros(shape=(trials+1, 2,ind)) # to store each trial number
-    MSEs = np.zeros(shape=(trials+1,2,ind)) # to store the MSE vs trials values
+    MSEs_rand = np.zeros(shape=(trials+1,2,ind)) # to store the MSE vs trials values
     designs = []
     results = []
     
@@ -232,17 +339,18 @@ for prior_type in list_priors:
             estimated = estimated.sum(axis = 1)
             # estimated=exp.values[np.argmax(exp.p,axis=1)]
             err = estimated - true
-            aux_err = np.mean(err**2)
+            aux_err = np.mean(err*err)
             MSE.append(aux_err)
             info.append(exp.infoProgress())
         aux1 = exp.ADOchoose()  
         designs.append(aux1)    
-        MSEs[:,jj,ii] = MSE
+        MSEs_rand[:,jj,ii] = MSE
         xs[:,jj,ii] = x
   
-    mse_random=np.mean([MSEs[:,1][-1]])
-    error_mse_random=np.std(MSEs[:,1][-1])/np.sqrt(ind)
+    mse_random=np.mean([MSEs_rand[:,1][-1]])
+    error_mse_random=np.std(MSEs_rand[:,1][-1])/np.sqrt(ind)
     plt.axhline(mse_random,linestyle='--',color='k',label='Random K=200')
     plt.axhline(mse_random-error_mse_random,color='grey')
     plt.axhline(mse_random+error_mse_random,color='grey')
     plt.show()
+
